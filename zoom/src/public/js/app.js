e969739f178332b84c3dfa0887f59a433874300a
a-honey <jungahyeon0512@khu.ac.kr>
2023-09-10 03:40:59 +0900

    :sparkles: Feat: socket.io를 이용한 채팅 구현
    닉네임 설정, 채팅방 입장, 메시지 보내기, 채팅방 퇴장

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,3 +1,7 @@
+/*
+
+@WebSocket
+
 const MessageBox = document.querySelector("ul");
 const MessageForm = document.querySelector("form");
 
@@ -29,3 +33,82 @@ MessageForm.addEventListener("submit", (e) => {
   socket.send(handleMessage("content", input.value));
   input.value = "";
 });
+*/
+
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const roomNameForm = welcome.querySelector("form");
+const room = document.getElementById("room");
+const nickName = document.getElementById("nickName");
+const nickNameForm = nickName.querySelector("form");
+
+// 첫 입장시 닉네임 입력창만 띄움
+room.hidden = true;
+welcome.hidden = true;
+
+let roomName;
+
+const handleMessageFrom = (msg) => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = msg;
+  ul.appendChild(li);
+};
+
+// roomName 채팅방 form 제출시 메시지 보내기 실행(argument: 채팅방 이름, 메시지내용, 나의 메시지 ul 추가 콜백)
+const handleMessageSubmit = (e) => {
+  e.preventDefault();
+
+  const input = room.querySelector("input");
+  const value = input.value; // 실행 시점 이전에 input.value가 초기화되기 때문에, 임시 보관
+  socket.emit("new_message", roomName, input.value, () => {
+    handleMessageFrom(`나: ${value}`);
+    input.value = "";
+  });
+};
+
+// roomName 입력 후 roomName 채팅방 입장
+const handleSubmitDone = (name) => {
+  welcome.hidden = true;
+  room.hidden = false;
+  roomName = name;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `${roomName} 채팅방`;
+
+  const form = room.querySelector("form");
+
+  // roomName 채팅방 form 제출시 메시지 보내기 실행
+  form.addEventListener("submit", handleMessageSubmit);
+};
+
+// 닉네임 입력 폼
+nickNameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  nickName.hidden = true;
+  welcome.hidden = false;
+
+  const input = nickNameForm.querySelector("input");
+  socket.emit("nickName", input.value);
+});
+
+// 채팅방 이름 입력 폼
+roomNameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const input = roomNameForm.querySelector("input");
+  socket.emit("enter_room", input.value, handleSubmitDone);
+  input.value = "";
+});
+
+socket.on("welcome", (nickName) => {
+  handleMessageFrom(`${nickName} 유저가 입장했습니다.`);
+});
+
+socket.on("bye", (nickName) => {
+  handleMessageFrom(`${nickName} 유저가 퇴장했습니다.`);
+});
+
+// 메시지를 받으면 메시지 ul 추가 콜백 실행
+socket.on("new_message", handleMessageFrom);
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
-import { WebSocket } from "ws";
+//import { WebSocket } from "ws";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -12,7 +13,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.render("/")); // 404 방지
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+/*
+
+@WebSocket
 
 // http 서버 생성
 const server = http.createServer(app);
@@ -26,9 +29,9 @@ wss.on("connection", (socket) => {
   sockets.push(socket);
   socket["nickname"] = "익명";
   console.log("Connected to Browser");
-
+  
   socket.on("close", () => console.log("Disconnected from Server"));
-
+  
   socket.on("message", (message) => {
     const msg = JSON.parse(message);
     switch (msg.type) {
@@ -37,13 +40,50 @@ wss.on("connection", (socket) => {
           item.send(`${socket.nickname}의 메시지: ${msg.payload}`);
         });
         break;
-      case "nickname":
-        socket["nickname"] = msg.payload;
-        break;
-      default:
-        break;
-    }
+        case "nickname":
+          socket["nickname"] = msg.payload;
+          break;
+          default:
+            break;
+          }
+        });
+      });
+      */
+
+const server = http.createServer(app);
+const wsServer = new Server(server);
+
+wsServer.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket["nickName"] = "익명유저";
+  // 이벤트 발생 시 항상 실행
+  socket.onAny((e) => {
+    console.log(`Socket Event: ${e}`);
+  });
+
+  // roomName 채팅방 입장
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done(roomName); // 클라이언트에서 argument로 보낸 함수를 '클라이언트'에서 실행시킴
+    socket.to(roomName).emit("welcome", socket.nickName); // 해당 room의 전체 유저에게 메시지를 보냄
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickName)
+    );
+  });
+
+  socket.on("new_message", (roomName, msg, done) => {
+    socket.to(roomName).emit("new_message", `${socket.nickName}: ${msg}`);
+    done();
+  });
+
+  socket.on("nickName", (nickName) => {
+    socket["nickName"] = nickName;
   });
 });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 server.listen(3000, handleListen);
