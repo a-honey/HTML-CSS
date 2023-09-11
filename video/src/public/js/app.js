84b6dc3961354f32902c4b996c500d5a79d9fa78
ahoney <jungahyeon0512@khu.ac.kr>
2023-09-11 14:59:12 +0900

    :bug: Bug: RTCPeerConnection 에러

--- video/src/public/js/app.js
+++ video/src/public/js/app.js
@@ -5,12 +5,19 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
-let myStream;
+const welcome = document.getElementById("welcome");
+const call = document.getElementById("call");
+
+call.hidden = true;  // 입장 전까지 hidden
+
+let myStream;  // 유저의 미디어 스트림
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;  // WebRTC 피어 커넥션
 
+// 카메라 목록을 가져오는 함수
 const getCameras = async () => {
-  console.log("카메라 목록을 가져옴");
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
@@ -31,8 +38,8 @@ const getCameras = async () => {
   }
 };
 
+// 미디어 스트림을 가져오는 함수(변경시 deviceId 미디어 스트림을 설정)
 const getMedia = async (deviceId) => {
-  console.log("카메라를 가져옴");
   const initialConstraints = {
     audio: false,
     video: { facingMode: "user" },
@@ -56,8 +63,6 @@ const getMedia = async (deviceId) => {
     console.log(err);
   }
 };
-
-getMedia();
 
 const handleMuteClick = () => {
   myStream
@@ -95,3 +100,97 @@ const handleCameraChange = async () => {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+welcomeForm = welcome.querySelector("form");
+
+// 방 입장 시 실행되는 함수
+const initCall = async () => {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();  // 나의 미디어 스트림을 설정
+  makeConnection();  // STUN 서버와 연결
+}
+
+// 방 입장 폼 제출 시 실행 함수
+const handleWelcomeSubmit = async (e) => {
+  e.preventDefault();
+
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);  // 방 입장 및 해당 방의 다른 유저들에게 welcome 이벤트 실행
+  roomName = input.value;
+  input.value= "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// 현재 방에 다른 유저가 들어왔을때 실행됨
+socket.on("welcome", async () => {
+  console.log("다른 유저가 들어옴")
+  // WebRTC 피어 간의 통신을 설정하고 제어하는데 사용되는 SDF 형식의 메시지(미디어 스트림 및 연결에 대한 정보를 포함)을 생성 및 설정
+  const offer = await myPeerConnection.createOffer();
+  console.log("나의 offer 생성", offer)
+  myPeerConnection.setLocalDescription(offer);  // 로컬 미디어 스트림을 설정
+  // 내 offer를 나를 제외한 방 유저에게 제공
+  socket.emit("offer", offer, roomName);
+})
+
+// 다른 유저가 들어와서 내 offer를 다른 유저에게 제공한 후, 다른 사람의 offer를 받으면서 실행
+socket.on("offer", async (offer) => {
+  console.log("받은 offer", offer)
+  // 원격 미디어 스트림(다른 유저의 offer)을 설정
+  myPeerConnection.setRemoteDescription(offer);
+  // 받은 offer를 기반으로 생성한 answer를 로컬 설명에 설정 후 answer을 전송
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+})
+
+// 다른 유저가 다른 유저의 offer를 받아 생성한 answer을 내 로컬에 설정
+socket.on("answer", (answer) => {
+  console.log("받은 answer", answer)
+  // 로컬 미디어 스트림에 대한 SDP 정보를 설정하는 메서드
+  myPeerConnection.setLocalDescription(answer);
+})
+
+socket.on("ice", (ice) => {
+  console.log("받은 ice", ice);
+  // ICE(Interactive Connectivity Establishment, P2P 연결을 설정하고 관리) 후보를 추가
+  myPeerConnection.addIceCandidate(ice);
+});
+
+const makeConnection = () => {
+  // 공용 주소를 사용하기 위해 STUN 서버를 사용(구글 무료 지원, 프로덕션의 경우 STUN 서버를 따로 생성해야함)
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ]
+      }
+    ] 
+  });
+
+  // ICE 프로토콜을 통해 P2P 연결을 설정하고 관리할 때 발생하는 이벤트
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  // 상대방의 미디어 스트림을 수신할 때 발생하는 이벤트
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  console.log(myStream.getTracks().forEach(track => myPeerConnection.addTrack(track,myStream)));
+}
+
+// 후보 정보가 존재할 경우, 다른 유저에게 전달(받은 유저는 후보를 추가)
+function handleIce(data) {
+    console.log("send candidate", data.candidate);
+    socket.emit("ice", data.candidate, roomName);
+}
+
+// 상대방의 미디어 스트림을 view
+function handleAddStream(data) {
+    console.log('상대방의 미디어 스트림을 보여줌')
+    const peersStream = document.getElementById("peerFace");
+    peersStream.srcObject = data.stream;
+}
--- video/src/server.js
+++ video/src/server.js
@@ -15,6 +15,29 @@ app.get("/*", (req, res) => res.render("/"));
 const server = http.createServer(app);
 const wsServer = new Server(server);
 
+wsServer.on("connection", (socket) => {
+    socket.onAny((e) => {
+        console.log(`Socket Event: ${e}`);
+      });
+
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        console.log(offer);
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        console.log(answer);
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        console.log(ice);
+        socket.to(roomName).emit("ice", ice);
+    });
+});
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 server.listen(3000, handleListen);
